2d1e0b0e546d590008524a12cb37c875fbe583cc
KatsunomOfficial <contact.katsunom@gmail.com>
2026-01-20 00:27:48 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-// Simple JS template
-console.log("Site loaded");
 
-// Smooth scroll for nav links
-document.querySelectorAll('a[href^="#"]').forEach(link => {
-  link.addEventListener('click', (e) => {
-    e.preventDefault();
-    const target = document.querySelector(link.getAttribute('href'));
-    if (target) {
-      target.scrollIntoView({ behavior: "smooth" });
-    }
-  });
-});
